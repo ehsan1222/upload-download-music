3fbb86caeeaaf5e19d98798040213558f44f42e5
ehsan1222 <s.e.maddahi.ir@gmial.com>
2020-03-03 22:30:37 +0330

    add dependency

---
+++ src/main/java/com/music/uploaddownloadmusic/UploadDownloadMusicApplication.java
@@ -0,0 +1,13 @@
+package com.music.uploaddownloadmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadDownloadMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadDownloadMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/music/uploaddownloadmusic/UploadDownloadMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.uploaddownloadmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadDownloadMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
