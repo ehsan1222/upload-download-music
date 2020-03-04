06ab4c82dba3f2267ceae0dee97a83dd3034602d
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-03-04 22:23:35 +0330

    fileNotFoundException Rename

--- src/main/java/com/music/uploaddownloadmusic/exception/MusicFileNotFoundException.java
+++ src/main/java/com/music/uploaddownloadmusic/exception/MusicFileNotFoundException.java
@@ -4,13 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class FileNotFoundException extends RuntimeException{
+public class MusicFileNotFoundException extends RuntimeException{
 
-    public FileNotFoundException(String message) {
+    public MusicFileNotFoundException(String message) {
         super(message);
     }
 
-    public FileNotFoundException(String message, Throwable cause) {
+    public MusicFileNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
 }
