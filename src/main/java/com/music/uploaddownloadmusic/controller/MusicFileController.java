f81c4d0fbd9cf223db421113a81dac785658f6f6
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-03-05 00:22:23 +0330

    get all music Records

--- src/main/java/com/music/uploaddownloadmusic/controller/MusicFileController.java
+++ src/main/java/com/music/uploaddownloadmusic/controller/MusicFileController.java
@@ -1,11 +1,15 @@
 package com.music.uploaddownloadmusic.controller;
 
+import com.music.uploaddownloadmusic.exception.MusicFileNotFoundException;
 import com.music.uploaddownloadmusic.model.MusicFile;
 import com.music.uploaddownloadmusic.payload.FirstPageMusicResponse;
 import com.music.uploaddownloadmusic.service.MusicFileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,8 +24,21 @@ public class MusicFileController {
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<FirstPageMusicResponse>> getAllMusicsWithoutAudioFiles () {
-        return null;
+    public ResponseEntity<List<FirstPageMusicResponse>> getAllMusicsWithoutAudioFiles() {
+        // Get all music Records
+        List<MusicFile> allMusics = musicFileService.getAllMusics();
+
+        // There was't music file
+        if (allMusics == null) {
+            throw new MusicFileNotFoundException("Music File NOT Found!");
+        }
+        List<FirstPageMusicResponse> firstPageMusicResponses = new ArrayList<>();
+        for (MusicFile musicFile : allMusics) {
+            FirstPageMusicResponse firstPageMusicResponse = new FirstPageMusicResponse
+                    (musicFile.getId(), musicFile.getFileName(), musicFile.getMusicFilePicture());
+            firstPageMusicResponses.add(firstPageMusicResponse);
+        }
+        return new ResponseEntity<>(firstPageMusicResponses, HttpStatus.OK);
     }
 
     @GetMapping("/{musicId}")
