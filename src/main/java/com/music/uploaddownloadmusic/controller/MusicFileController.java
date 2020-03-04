f5ee50c7550197d4948a04b4ab19cb4e3daedb82
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-03-04 21:25:05 +0330

    music controller backbone

--- src/main/java/com/music/uploaddownloadmusic/controller/MusicFileController.java
+++ src/main/java/com/music/uploaddownloadmusic/controller/MusicFileController.java
@@ -1,9 +1,48 @@
 package com.music.uploaddownloadmusic.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.music.uploaddownloadmusic.model.MusicFile;
+import com.music.uploaddownloadmusic.payload.FirstPageMusicResponse;
+import com.music.uploaddownloadmusic.service.MusicFileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
+@RequestMapping("/musics")
 public class MusicFileController {
 
+    private MusicFileService musicFileService;
+
+    @Autowired
+    public MusicFileController(MusicFileService musicFileService) {
+        this.musicFileService = musicFileService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<FirstPageMusicResponse>> getAllMusicsWithoutAudioFiles () {
+        return null;
+    }
+
+    @GetMapping("/{musicId}")
+    public ResponseEntity<MusicFile> getMusicFile(@PathVariable("musicId") Long musicId) {
+        return null;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<FirstPageMusicResponse>> getSearchedMusicFile(@RequestParam("musicName") String musicName) {
+        return null;
+    }
+
+    @PutMapping("/{musicId}")
+    public ResponseEntity<MusicFile> updateMusicFile(@PathParam("musicId") Long musicId, @RequestBody MusicFile musicFile) {
+        return null;
+    }
+
+    @DeleteMapping("/{musicId}")
+    public ResponseEntity<?> deleteMusicFile(@PathVariable("musicId") Long musicId) {
+        return null;
+    }
 
 }
