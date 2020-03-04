863afc85ec15eb0b0ab47a73cf5a5f2378eea179
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-03-04 21:44:33 +0330

    change pathParam to pathVariable

--- src/main/java/com/music/uploaddownloadmusic/controller/MusicFileController.java
+++ src/main/java/com/music/uploaddownloadmusic/controller/MusicFileController.java
@@ -6,7 +6,6 @@ import com.music.uploaddownloadmusic.service.MusicFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -36,7 +35,7 @@ public class MusicFileController {
     }
 
     @PutMapping("/{musicId}")
-    public ResponseEntity<MusicFile> updateMusicFile(@PathParam("musicId") Long musicId, @RequestBody MusicFile musicFile) {
+    public ResponseEntity<MusicFile> updateMusicFile(@PathVariable("musicId") Long musicId, @RequestBody MusicFile musicFile) {
         return null;
     }
 
